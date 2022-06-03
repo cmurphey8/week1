35532c52e8e33acb8de2a7c3d233a4cc37686ba8
cmurphey8 <53098445+cmurphey8@users.noreply.github.com>
2022-06-03 09:26:20 -0400

    Create helpers.c

---
+++ helpers.c
@@ -0,0 +1,19 @@
+// WE WILL WRITE OUR OWN STRING HELPER METHODS!
+// DO NOT IMPORT ADDITIONAL LIBRARIES FOR THIS EXERCISE
+#include <stdio.h>
+#include <stdbool.h>
+#include <cs50.h>
+
+// TODO: update helper prototypes with parameter and return types
+// HINT: check the CS50 manual for reference!
+void isalpha(void);
+void toupper(void);
+void strlen(void);
+
+int main()
+{
+    // TODO: write tests for each of your method as you complete them
+
+    // eg,  string str = get_string("test string: ");
+    //      printf("length of string is %i", strlen(str));
+}
